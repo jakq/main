19cba822cf9380880321ae71b058f7f256d153ce
jakq <42934003+jakq@users.noreply.github.com>
2019-10-01 23:07:51 +0800

    create new command remark

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -22,9 +22,9 @@ import seedu.address.model.person.Person;
 public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline").withEmail("alice@example.com")
-            .withPhone("94351253").withTags("friends").build();
+            .withPhone("94351253").withRemark("She likes aardvarks.").withTags("friends").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier").withEmail("johnd@example.com")
-            .withPhone("98765432").withTags("owesMoney", "friends").build();
+            .withPhone("98765432").withRemark("He can't take beer!").withTags("owesMoney", "friends").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
